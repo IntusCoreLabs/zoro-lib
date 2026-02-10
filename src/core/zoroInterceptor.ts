dc83ee8ace488bb04e78e597017b3c667b8ef04b
watercuz <eurysosagarcia@gmail.com>
2026-02-10 11:54:24 -0400

    feat: implementing request interceptor, safe and generic typing

--- src/core/zoroInterceptor.ts
+++ src/core/zoroInterceptor.ts
@@ -1,1 +1,9 @@
-console.log('hi')
+import { Zoro } from "./zoroHttp.ts";
+import { InterceptorManager } from "./zoroInterceptorManager.ts";
+
+export class Interceptor extends Zoro {
+  public interceptors = {
+    request: new InterceptorManager(),
+    response: new InterceptorManager(),
+  };
+}
