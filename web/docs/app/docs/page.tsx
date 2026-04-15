180feec9d23925908c6949fe784ecff95c2defd1
Eury sosa Garcia <eurysosagarcia@gmail.com>
2026-04-15 19:20:47 -0400

    feat: linter & formatting

--- web/docs/app/docs/page.tsx
+++ web/docs/app/docs/page.tsx
@@ -1,16 +1,16 @@
-import type { Route } from './+types/page';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/layouts/docs/page';
-import { source } from '@/lib/source';
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import browserCollections from 'fumadocs-mdx:collections/browser';
-import { baseOptions } from '@/lib/layout.shared';
-import { useFumadocsLoader } from 'fumadocs-core/source/client';
+import type { Route } from "./+types/page";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { DocsBody, DocsDescription, DocsPage, DocsTitle } from "fumadocs-ui/layouts/docs/page";
+import { source } from "@/lib/source";
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import browserCollections from "fumadocs-mdx:collections/browser";
+import { baseOptions } from "@/lib/layout.shared";
+import { useFumadocsLoader } from "fumadocs-core/source/client";
 
 export async function loader({ params }: Route.LoaderArgs) {
-  const slugs = params['*'].split('/').filter((v) => v.length > 0);
+  const slugs = params["*"].split("/").filter((v) => v.length > 0);
   const page = source.getPage(slugs);
-  if (!page) throw new Response('Not found', { status: 404 });
+  if (!page) throw new Response("Not found", { status: 404 });
 
   return {
     path: page.path,
